6b30a12d083957c2c9fafc30e0064ec3edf30f83
Kolisnyk <Kolisnyk@inbox.ru>
2016-10-02 20:09:51 +0300

    Blog  with   registration

---
+++ db/migrate/20161002153029_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.string :image_file_name
+      t.string :image_content_type
+      t.integer :image_file_size
+      t.datetime :image_updated_at
+      t.timestamps null: false
+    end
+  end
+end
