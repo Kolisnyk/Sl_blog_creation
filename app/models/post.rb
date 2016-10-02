ba7c8ac15177ebee9a601e2801c7d106cb4f858e
Kolisnyk <Kolisnyk@inbox.ru>
2016-10-02 22:13:38 +0300

    added users images

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+
+belongs_to :user
+
+has_attached_file :image,
+:styles => {
+			:thumb => "20x20#",
+			:small => "56x56>",
+			:medium => "165x180>",
+			:medium => "640x480>"},
+			:default_style => :medium,
+			
+			:url => "/images/posts/:id-:basename-:style.:extension",
+			:path => ":rails_root/public/images/posts/:id-:basename-:style.:extension"
+
 end
